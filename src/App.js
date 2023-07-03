c297bbbbe96370021eaf5a722c1d1b9131f0d8f6
Harkaran-Singh-Bal <138457823+Harkaran-Singh-Bal@users.noreply.github.com>
2023-07-03 19:07:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import Header from './Header'
+import Home from './Home'
+import Checkout from './Checkout'
+import Login from './Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { auth } from './Firebase';
+import { useStateValue } from './StateProvider';
+
+function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run once when the app component loads
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in or the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+  return (
+    //BEM
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/checkout' element={<><Header /><Checkout /></>} />
+          <Route path='/' element={<><Header /><Home /></>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
